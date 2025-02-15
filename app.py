1111e2200a4a0f8d12d1ccc997f779cf9bfe0601
Malcolm KOH <malcolm5964@gmail.com>
2025-02-16 02:09:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def db_status():
         return jsonify({"error": "Database connection failed"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
